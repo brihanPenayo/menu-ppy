8a57083e9e5aae03a037527b7a2f1ee1ef8bea83
brihanPenayo <brihan.penayos@gmail.com>
2022-11-01 23:51:43 -0300

    Menu PPY que lee datos desde GoogleSheets

---
+++ script.js
@@ -0,0 +1,62 @@
+const SECCION_HAMBURGUESA = document.getElementById('productoHamburguesas');
+const SECCION_PIZZA = document.getElementById('productoPizzas');
+const SECCION_OTROS = document.getElementById('productosOtros');
+
+const LISTA = document.getElementById('lista-menu');
+const SHEET_ID = "1psIyB5-x-eyziTDoC2l5Y_V4hNtF29LMDQ8i5k3f5h0";
+const ACCESS_TOKEN =
+  "ya29.a0Aa4xrXNW0c4dwkt-_s7y7kzhds1pHOMEF8KO5_0Rpr379_bbFMFBS3lOR5cSg8whye6CxK486yNoZ2YTLeTV460ngj8lga5ji5VMWMW0L6z_PzZBItc38Mdw2_6hIZz_TqclwScT6t8J4rU7RqvE9J4Hqp_2xwaCgYKATASARESFQEjDvL9qlYRWKWYwxfdsTtPbg-JlA0165";
+
+fetch(
+  // Obtenemos los datos de la planilla, de la hoja hojaMenu, columnas A y B desde la segunda fila
+  `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/menu!A1:D`,
+  {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${ACCESS_TOKEN}`
+    }
+  }
+).then(function (response) {
+  if (response.status >= 400) {
+    alert('NO SE PUDO CONECTAR')
+  }
+  response.json().then((data) => {
+    localStorage = data.json;
+    const VALORES = data.values;
+    consultarTipo(VALORES);
+    console.log(localStorage);
+  });
+})
+
+function consultarTipo(dato) {
+  for (x = 0; x < dato.length; x++) {
+    if (dato[x][0] === "HAMBURGUESA") {
+      document.getElementById('hamburguesas').hidden = false;
+      document.getElementById('hamburguesas').innerHTML = "HAMBURGUESAS";
+      SECCION_HAMBURGUESA.innerHTML += devolverProduvto(dato[x]);
+      console.log(dato[x])
+    }
+    else if (dato[x][0] === "PIZZA") {
+      document.getElementById('pizzas').hidden = false;
+      document.getElementById('pizzas').innerHTML = "PIZZAS";
+      SECCION_PIZZA.innerHTML += devolverProduvto(dato[x]);
+    }
+    else if (dato[x][0] === "OTROS") {
+      document.getElementById('otros').hidden = false;
+      document.getElementById('otros').innerHTML = "OTROS";
+      SECCION_OTROS.innerHTML += devolverProduvto(dato[x]);
+    }
+  }
+}
+
+function devolverProduvto(Producto) {
+  return `<div class="item"><span class="nombre">${Producto[1]}</span>${devolverPrecio(Producto)}${devolverDescripcion(Producto)}</div>`;
+}
+
+function devolverPrecio(Precio) {
+  return `<span class="precios">${Precio[3]}</span>`;
+}
+
+function devolverDescripcion(Descripcion) {
+  return `<p>${Descripcion[2]}</p>`;
+}
